4646ec5279255228f0b6688de884f81c8e893e3a
Emma Zappala <emmazappala@emmas-mbp-2.lan>
2021-09-16 19:36:58 -0600

    bug fixes

--- CommentAutomaton.cpp
+++ CommentAutomaton.cpp
@@ -10,11 +10,11 @@ void CommentAutomaton::S0(const std::string& input) {
         substring = "";
         if (input[index] == '|') {
             index++;
-            std::cout << "going to state 2" << std::endl;
+            //std::cout << "going to state 2" << std::endl;
             CommentAutomaton::S2(input);
 
         } else {
-            std::cout << "going to state 1" << std::endl;
+            //std::cout << "going to state 1" << std::endl;
             CommentAutomaton::S1(input);
 
 
@@ -27,7 +27,7 @@ void CommentAutomaton::S0(const std::string& input) {
 void CommentAutomaton::S1(const std::string& input) {
     substring = "#";
     while(input[index]!='\n'){
-        std::cout << "looking at " << input[index] << std::endl;
+        //std::cout << "looking at " << input[index] << std::endl;
         substring.push_back(input[index]);
         index++;
         inputRead++;
@@ -38,15 +38,15 @@ void CommentAutomaton::S2(const std::string& input) {
     substring = "#|";
     bool run = true;
     while(run) {
-        std::cout << "looking at " << input[index] << " in state 2." << std::endl;
+        //std::cout << "looking at " << input[index] << " in state 2." << std::endl;
         substring.push_back(input[index]);
         if(input[index]=='|'){
-            std::cout << "equal to |" << std::endl;
+            //std::cout << "equal to |" << std::endl;
             if(input[index+1]=='#'){
                 inputRead++;
                 inputRead++;
                 inputRead++;
-                std::cout << " here we found the end of a comment woot woot cool" << std::endl;
+                //std::cout << " here we found the end of a comment woot woot cool" << std::endl;
                 break;
             }
         }
